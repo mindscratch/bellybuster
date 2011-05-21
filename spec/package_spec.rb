36652e1c5ecd66d1684c37c1e90198cb5c43aba3
Craig Wickesser <craig@mindscratch.org>
2011-05-21 00:23:32 -0400

    some initial specs

--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -1,7 +1,18 @@
 require File.expand_path("../spec_helper", __FILE__)
 
-BellyBuster.describe "Package#to_hash" do
-  should "provide a hash with any extra properties given" do
+describe BellyBuster::Package do
+
+  it "should always have a content" do
+    package = BellyBuster::Package.new
+    package[:content].should be_nil
+  end
+
+  it "should always have a type" do
+    package = BellyBuster::Package.new
+    package[:type].should == 'rb'
+  end
+
+  it "should provide a hash with any extra properties given" do
     package = BellyBuster::Package.new
     package[:version] = 1
     hash = package.to_hash
